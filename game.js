9fdde5385955f5b24bfbebd74f7ea80d7bb1d7db
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-04 00:24:03 +0300

    addition of alerts

--- game.js
+++ game.js
@@ -3,39 +3,54 @@ window.addEventListener("load", function () {
     
     const start = document.getElementById("start");
     const boundaries = document.getElementsByClassName("boundary");
-    const end = this.document.getElementById("end");
-    console.log(result)
+    const end = document.getElementById("end");
+    const smallBox = document.getElementsByClassName("example");
+    
     var score=0 ;
 
     start.addEventListener("pointerover",play );
 
     function play () {       
         start.style.color="blue";
+        document.getElementById("status").innerHTML= "Click on 'E' box to win"
 
         for(var i=0; i< boundaries.length; i++){
-            boundaries[i].addEventListener("pointerover",lose);
+            boundaries[i].addEventListener("pointerover",lose);     
         }
         function lose( ){
             for( var z=0; z<boundaries.length; z++){
                 boundaries[z].style.background= "red"  ; 
+                if(boundaries[z].addEventListener("pointerover",alert("Game Over"))){
+                    return
+                }
                 document.getElementById("result").innerHTML= "GAME OVER :("
                 
             }
+            smallBox.style.background="white" ;
+            
+
         }
         end.addEventListener("pointerover",finish);
         function finish () {
             end.style.color="blue";
+            end.addEventListener("click",alert("CONGRATS YOU WIN "))
             document.getElementById("result").innerHTML= "CONGRATS YOU WON!!"
         }
-    
+        start.addEventListener("click",reset);
+        function reset ( ){
+            start.style.color="black";
+            end.style.color="black";
+            for( var z=0; z<boundaries.length; z++){
+                boundaries[z].style.background= "#eeeeee"  ; 
+                document.getElementById("result").innerHTML= "Good luck!"
+
+            }
+        }
 
         
 
     
-    // start.addEventListener("click",reset);
-    // function reset ( ){
-
-    // }
+    
         
   }
 });
